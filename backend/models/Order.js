7d4412da163d327d22147e12877cd7ed4aa3208c
Riya Girdhar <riyagirdhar30@gmail.com>
2026-07-07 00:10:07 +0530

    Added payment method and payment status to order schema

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -2,23 +2,37 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
-   products: [
-  {
-    name: String,
-    price: Number,
-    quantity: {
-      type: Number,
-      default: 1,
-    },
-  },
-],
+    products: [
+      {
+        name: String,
+        price: Number,
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
 
     userEmail: {
-  type: String,
-  required: true,
-},
+      type: String,
+      required: true,
+    },
 
     totalPrice: Number,
+
+    // ✅ NEW
+    paymentMethod: {
+      type: String,
+      enum: ["Online", "COD"],
+      default: "Online",
+    },
+
+    // ✅ NEW
+    paymentStatus: {
+      type: String,
+      enum: ["Paid", "Pending"],
+      default: "Pending",
+    },
 
     status: {
       type: String,
